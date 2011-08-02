e925a894a0b24a7bd18b484a9bb7b79b0e32a283
Luis Nell <ln@nells.eu>
2011-08-03 00:14:28 +0200

    forgot to add the parsers.py *cough cough*

---
+++ sorl/watermark/parsers.py
@@ -0,0 +1,13 @@
+from sorl.thumbnail.parsers import parse_geometry as xy_geometry_parser
+
+def parse_geometry(geometry, ratio=None):
+    """
+    Enhanced parse_geometry parser with percentage support.
+    """
+    if not '%' in geometry:
+        # fall back to old parser
+        xy_geometry_parser(geometry, ratio)
+
+    # parse with float so geometry strings like "42.11%" are possible
+    return float(geometry.strip('%'))/100.0
+
