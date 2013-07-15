93fb8f9e0d1f0b19e727d0bb2521d96217ebe3d8
vbazhin <v.bazhin@gmail.com>
2013-07-15 15:44:12 +1100

    common methods moved from pil to base engine

--- sorl_watermarker/engines/base.py
+++ sorl_watermarker/engines/base.py
@@ -5,12 +5,13 @@ from django.contrib.staticfiles.finders import find
 from sorl.thumbnail.engines.base import EngineBase as ThumbnailEngineBase
 from sorl_watermarker.parsers import parse_geometry
 from sorl_watermarker.conf import settings
-
+from django.core.exceptions import ImproperlyConfigured
 
 class WatermarkEngineBase(ThumbnailEngineBase):
     """
     Extend sorl.thumbnail base engine to support watermarks.
     """
+
     def create(self, image, geometry, options):
         image = super(WatermarkEngineBase, self).create(image, geometry,
                                                         options)
@@ -21,6 +22,7 @@ class WatermarkEngineBase(ThumbnailEngineBase):
                 'watermark_alpha' in options):
             image = self.watermark(image, options)
         return image
+
 
     def watermark(self, image, options):
         """
@@ -53,4 +55,50 @@ class WatermarkEngineBase(ThumbnailEngineBase):
 
         return self._watermark(image, watermark_path,
                                options['watermark_alpha'],
-                               options['watermark_size'], options['watermark_pos'])
+                               options['watermark_size'],
+                               options['watermark_pos'])
+
+
+    def _get_watermark_size(self, size, mark_default_size):
+        """
+            New size can be passed as a pair of valuer (tuple) or
+            a float (persentage case)
+        """
+        if hasattr(size, '__getitem__'):
+            # a tuple or any iterable already
+            mark_size = size
+        elif isinstance(size, float):
+            mark_size = map(lambda coord: int(coord*size), mark_default_size)
+            # TODO: Might be useful to expose the crop/upscale options
+            #       to django settings
+        else:
+            raise ImproperlyConfigured('Watermark sizes must be a pair '
+                                       'of integers or a float number')
+        return mark_size
+
+
+    def _define_watermark_position(self, position_string, im_size, mark_size):
+        pos_list = position_string.split(' ')
+        coords = {'x': {'west': 0,
+                        'east': im_size[0] - mark_size[0]},
+                  'y': {'north': 0,
+                        'south': im_size[1] - mark_size[1]}, }
+        # if values can be parsed as numeric
+        try:
+            x_abs = int(pos_list[0])
+            y_abs = int(pos_list[1])
+            # values below 0
+            x_pos = x_abs if x_abs >= 0 else coords['x']['east'] + x_abs
+            y_pos = y_abs if y_abs >= 0 else coords['y']['south'] + y_abs
+            position = (x_pos, y_pos)
+        # if the values are not a pair of numbers
+        except ValueError:
+            if pos_list == ['center']:
+                position = (coords['x']['east']/2, coords['y']['south']/2)
+            else:
+                x_val = [lon for lon in pos_list if lon in coords['x']]
+                y_val = [lat for lat in pos_list if lat in coords['y']]
+                x_key = x_val[0] if len(x_val) > 0 else 'east'
+                y_key = y_val[0] if len(y_val) > 0 else 'south'
+                position = (coords['x'][x_key], coords['y'][y_key])
+        return position
--- sorl_watermarker/engines/pil.py
+++ sorl_watermarker/engines/pil.py
@@ -36,18 +36,6 @@ class Engine(WatermarkEngineBase, PILEngine):
         layer.paste(watermark, position)
         return Image.composite(layer, image, layer)
 
-    def _get_watermark_size(self, size, mark_default_size): 
-        if hasattr(size, '__getitem__'):
-            # a tuple or any iterable already
-            mark_size = size
-        elif isinstance(size, float):
-            mark_size = map(lambda coord: int(coord*size), mark_default_size)
-            # TODO: Might be useful to expose the crop/upscale options
-            #       to django settings
-        else:
-            raise ImproperlyConfigured('Watermark sizes must be a pair '
-                                       'of integers or a float number')
-        return mark_size
 
     def _reduce_watermark_opacity(self, image, opacity):
         if image.mode != 'RGBA':
@@ -58,30 +46,4 @@ class Engine(WatermarkEngineBase, PILEngine):
         alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
         image.putalpha(alpha)
         return image
-
-    def _define_watermark_position(self, position_string, im_size, mark_size):
-        pos_list = position_string.split(' ')
-        coords = {'x': {'west': 0,
-                        'east': im_size[0] - mark_size[0]},
-                  'y': {'north': 0,
-                        'south': im_size[1] - mark_size[1]}, }
-        # if values can be parsed as numeric
-        try:
-            x_abs = int(pos_list[0])
-            y_abs = int(pos_list[1])
-            # values below 0
-            x_pos = x_abs if x_abs >= 0 else coords['x']['east'] + x_abs
-            y_pos = y_abs if y_abs >= 0 else coords['y']['south'] + y_abs
-            position = (x_pos, y_pos)
-        # if the values are not a pair of numbers
-        except ValueError:
-            if pos_list == ['center']:
-                position = (coords['x']['east']/2, coords['y']['south']/2)
-            else:
-                x_val = [lon for lon in pos_list if lon in coords['x']]
-                y_val = [lat for lat in pos_list if lat in coords['y']]
-                x_key = x_val[0] if len(x_val) > 0 else 'east'
-                y_key = y_val[0] if len(y_val) > 0 else 'south'
-                position = (coords['x'][x_key], coords['y'][y_key])
-        return position
 
