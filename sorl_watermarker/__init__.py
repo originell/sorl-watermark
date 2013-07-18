820e57f5d65fd463ad9c558e01f310cfdd185a11
vbazhin <v.bazhin@gmail.com>
2013-07-19 00:02:59 +1100

    deleted watermark engine autoinit

--- sorl_watermarker/__init__.py
+++
@@ -1,5 +0,0 @@
-from sorl.thumbnail.conf import defaults as thumb_defaults
-from sorl_watermarker.conf import defaults as watermark_defaults
-
-# replaces the default sorl-thumbnail engine with the sorl-watermarker default one
-setattr(thumb_defaults, 'THUMBNAIL_ENGINE', watermark_defaults.THUMBNAIL_ENGINE)
