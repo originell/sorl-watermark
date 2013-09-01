486dd5ec726b321236238a021282563d2b829674
vbazhin <v.bazhin@gmail.com>
2013-09-02 07:58:25 +1100

    pgmagick teardown auto clean up

--- tests/engines/pgmagick_test.py
+++ tests/engines/pgmagick_test.py
@@ -3,25 +3,40 @@ from pgmagick import Image as PGImage
 from PIL import Image as PILImage
 from base_case import BaseCase
 import unittest
-
+import os
 
 
 from sorl_watermarker.engines.pgmagick_engine import Engine as PGEngine
 
 class PGmagickTestCase(unittest.TestCase, BaseCase):
 
+
     def setUp(self):
         self.engine = PGEngine()
         self.bg_path = 'src/bg.png'
-        self.temp_path = 'src/temp/temp.png'
+        self.temp_dir = 'src/temp/'
         self.img_dir = 'src/control_instances/created_with_pil/png/'
+        if not os.path.exists(self.temp_dir):
+            os.mkdir(self.temp_dir)
+
 
     def get_comparable_image(self, options):
         bg = PGImage(self.bg_path)
         marked_image = self.engine.watermark(bg, options)
-        marked_image.write(self.temp_path + 'temp.png')
-        mark = PILImage.open(self.temp_path + 'temp.png')
+        temp_image_path = self.temp_dir + str(options.keys()[0]) + \
+                          "_" + str(options.values()[0]) + '.png'
+        marked_image.write(temp_image_path)
+        mark = PILImage.open(temp_image_path)
+        os.remove(temp_image_path)
         return mark
+
+
+
+    def tearDown(self):
+        if os.path.exists(self.temp_dir):
+            os.rmdir(self.temp_dir)
+
+
 
 
 def suite():
