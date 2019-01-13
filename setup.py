dac3fc4d4574cf0171d9f7b619c499a707ff7ce0
Luis Nell <luis.nell@simpleloop.com>
2019-01-13 19:37:05 +0100

    increase version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name="sorl-watermark",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/originell/sorl-watermark",
     author="Luis Nell",
     author_email="cooperate@originell.org",
