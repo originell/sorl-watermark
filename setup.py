6768865d400e3722564f6c5d1bedecc7bd5686ca
Luis Nell <luis.nell@codista.com>
2021-08-24 19:26:00 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-# coding: utf-8
+from codecs import open
+from os import path
 
-import os
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import Extension, find_packages, setup
 
+__version__ = "2.0.0"
 
-README_FPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
     name="sorl-watermark",
-    version="1.2.0",
-    url="https://github.com/originell/sorl-watermark",
-    author="Luis Nell",
-    author_email="luis@originell.org",
-    packages=find_packages(),
-    platforms="any",
-    description="Image based watermarks for sorl-thumbnail",
-    long_description=open(README_FPATH).read(),
+    version=__version__,
+    description="Adds support for watermarks to sorl-thumbnail",
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/originell/sorl-watermark",
+    download_url="https://github.com/originell/sorl-watermark/tarball/" + __version__,
+    license="MIT",
+    keywords="django sorl thumbnail watermark",
     classifiers=[
         "Development Status :: 6 - Mature",
         "Environment :: Web Environment",
@@ -27,8 +31,17 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Multimedia :: Graphics",
     ],
-    install_requires=["sorl-thumbnail"],
+    packages=find_packages(exclude=["docs", "tests*"]),
+    include_package_data=True,
+    author="Luis Nell",
+    author_email="luis@originell.org",
+    install_requires=["sorl-thumbnail>=12.4.1"],
 )
