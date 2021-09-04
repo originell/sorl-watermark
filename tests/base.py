28c001a91a18ccb3739f4002b7512552581eba3b
Luis Nell <luis.nell@codista.com>
2021-09-04 23:42:32 +0200

    feat: vips engine tests.

--- tests/base.py
+++ tests/base.py
@@ -87,7 +87,7 @@ def get_expected_image(
     * ``engine="wand"`` will return a Wand Image
     """
     image_path = get_expected_image_path(option, value, engine)
-    if engine.lower() in ["pgmagick", "pil", "pillow", "convert"]:
+    if engine.lower() in ["pgmagick", "pil", "pillow", "convert", "vips"]:
         # https://github.com/python-pillow/Pillow/issues/835
         with open(image_path, "rb") as image_file:
             with PILImage.open(image_file) as verification_img:
