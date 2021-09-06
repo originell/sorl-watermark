7ed6d78ec0e4f7b361f0d7f47345c82902b8615e
Luis Nell <luis.nell@codista.com>
2021-09-07 00:31:49 +0200

    chore: adds project urls to proj spec.

--- setup.py
+++ setup.py
@@ -20,6 +20,12 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/originell/sorl-watermark",
     download_url="https://github.com/originell/sorl-watermark/tarball/" + __version__,
+    project_urls={
+        "Source": "https://github.com/originell/sorl-watermark",
+        "Release notes": "https://github.com/originell/sorl-watermark/releases",
+        "Changelog": "https://github.com/originell/sorl-watermark/blob/main/CHANGELOG.md",
+        "Issues": "https://github.com/originell/sorl-watermark/issues",
+    },
     license="MIT",
     keywords="django sorl thumbnail watermark",
     classifiers=[
